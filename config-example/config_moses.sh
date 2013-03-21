a13623d698f9600efbace23ae8e5ec43bd0156db
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2013-03-21 17:43:10 +0400

    Fixes

--- config-example/config_moses.sh
+++ config-example/config_moses.sh
@@ -1,5 +1,7 @@
-export RECASER_PORT=9000
-export RECASER_INI=~khresmoi/models-stable/ende/recaser.moses.ini
+export MODELS_DIR=/home/khresmoi/models-stable/deen
 
-export TRANSL_PORT=8080
-export TRANSL_INI=~khresmoi/models-stable/ende/moses.ini
+export RECASER_PORT=9001
+export RECASER_INI=$MODELS_DIR/recaser.moses.ini
+
+export TRANSL_PORT=8081
+export TRANSL_INI=$MODELS_DIR/moses.ini
