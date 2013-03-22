b9e5125e873625c717f283d0b1ad5c6cbbcfd102
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2013-03-22 15:10:31 +0400

    Virtualenv install notes

--- virtualenv/install.sh
+++ virtualenv/install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 #
-# Installation of mt-worker environment.
+# Installation of the Python environment for workers and appserver.
 #
+# Assuming ~khresmoi/virtualenv as the target directory, 
+# and Ubuntu (tested on 10.04) with sudo possibility 
+# or python-dev package installed.
+
+cd ~khresmoi
+
+# Check if we have python-dev installed
+dpkg -s python-dev || sudo apt-get install python-dev
+
 # Download virtualenv
 wget https://raw.github.com/pypa/virtualenv/master/virtualenv.py
 
@@ -12,3 +21,6 @@ python virtualenv.py virtualenv
 # Activate virtualenv and install needed libraries
 source virtualenv/bin/activate
 pip install flask validictory regex
+
+# clean up
+rm virtualenv virtualenv.py
