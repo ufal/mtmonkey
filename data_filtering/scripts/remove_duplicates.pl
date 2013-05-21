9192fd356dcb31e6257b09697507d50d75c30f58
Michal Novak <mnovak@ufal.mff.cuni.cz>
2013-05-21 16:33:30 +0200

    duplicates removed before the split is performed

--- data_filtering/scripts/remove_duplicates.pl
+++ data_filtering/scripts/remove_duplicates.pl
@@ -3,15 +3,20 @@
 use strict;
 use warnings;
 
+use JSON;
 use Digest::SHA qw(sha1);
 
 my %text_hash = ();
 
 while (my $line = <STDIN>) {
-    my $sha_sum = sha1($line);
+    chomp $line;
+    my $decoded_json = JSON::decode_json( $line );
+    my $text = $decoded_json->{'doc'}{'htmlContent'}{'text'};
+    
+    my $sha_sum = sha1($text);
     #print STDERR $sha_sum . "\n";
     if (!defined $text_hash{$sha_sum}) {
-        print $line;
+        print $line."\n";
         $text_hash{$sha_sum}++;
     }
 }
