94461b544d118a46b33c0929ed7a684e3ba78901
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2013-09-27 16:36:54 +0200

    Fixes to prepare_appserver

--- install/prepare_appserver.sh
+++ install/prepare_appserver.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+#
+# Copying all that is needed for appserver environment
+#
+# Assuming ~$USER as the target directory. 
+#
+# This will create the following directories:
+#
+# ~$USER/appserver-$VERSION = main directory for the appserver,
+#   with the following subdirectories:
+#
+#   git/        = the Git repository of MTMonkey
+#   scripts     = link to scripts directory in Git
+#   appserver   = link to appserver/src directory in Git
+#   config/     = directory for configuration files
+#   logs/       = directory for log files
+#
+# Note that this will neither install Python virtualenv nor adjust
+# the configuration in ~$USER/appserver-$VERSION/config/appserver.cfg .
 
 if [[ -z "$VERSION" || -z "$USER" ]]; then
     echo "Usage: USER=username VERSION=version-name  prepare_appserver.sh"
@@ -18,5 +37,5 @@ ln -s git/appserver/src appserver
 ln -s git/scripts scripts
 
 # copy default config
-cp git/config-example/{appserver.cfg} config
+cp git/config-example/appserver.cfg config
 
