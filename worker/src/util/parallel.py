23ae80faf2b03cdba3233e884c3bcd92c9fd6f2d
Ales Tamchyna <tamchyna@ufal.mff.cuni.cz>
2016-01-14 18:24:19 +0100

    attempt to fix resource leak in parallel_map

--- worker/src/util/parallel.py
+++ worker/src/util/parallel.py
@@ -11,15 +11,17 @@ from multiprocessing import Process, Pipe
 from itertools import izip
 
 def spawn(f):
-    def fun(pipe,x):
-        pipe.send(f(x))
-        pipe.close()
+    def fun(ppipe, cpipe,x):
+        ppipe.close()
+        cpipe.send(f(x))
+        cpipe.close()
     return fun
 
 def parallel_map(f,X):
     pipe=[Pipe() for x in X]
-    proc=[Process(target=spawn(f),args=(c,x)) for x,(p,c) in izip(X,pipe)]
+    proc=[Process(target=spawn(f),args=(p,c,x)) for x,(p,c) in izip(X,pipe)]
     [p.start() for p in proc]
+    ret = [p.recv() for (p,c) in pipe]
     [p.join() for p in proc]
-    return [p.recv() for (p,c) in pipe]
+    return ret
 
